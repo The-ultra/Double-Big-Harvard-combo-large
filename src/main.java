6401c3672a35992a33a4a2c7becd1b88f105612c
The-ultra <40963403+The-ultra@users.noreply.github.com>
2021-06-13 11:51:45 +0200

    The base of operation
    I just made Arrays as required

---
+++ src/main.java
@@ -0,0 +1,26 @@
+public class main {
+
+
+
+    Object[] instructionMemory  = new Object[1023];
+    Object[] dataMemory         = new Object[2047];
+    Object[] Registers          =   new Object[66];
+       int[] statusReg          =       new int[7];
+
+
+    public void fetch(){
+
+    }
+    public void execute(){
+
+}
+
+
+
+
+
+
+
+
+
+}
