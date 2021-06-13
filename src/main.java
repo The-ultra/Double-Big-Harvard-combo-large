860ad2ddafffae9296dea3c3f34789502376b1f7
The-ultra <40963403+The-ultra@users.noreply.github.com>
2021-06-13 11:55:41 +0200

    Fine Tuning stuff

--- src/main.java
+++ src/main.java
@@ -1,26 +1,23 @@
 public class main {
 
 
+    Object[] instructionMemory = new Object[1023];
+    Object[] dataMemory = new Object[2047];
+    Object[] Registers = new Object[63];
+    int[] statusReg = new int[7];
+    int programCounter = 0;
 
-    Object[] instructionMemory  = new Object[1023];
-    Object[] dataMemory         = new Object[2047];
-    Object[] Registers          =   new Object[66];
-       int[] statusReg          =       new int[7];
-
-
-    public void fetch(){
+    public void fetch() {
 
     }
-    public void execute(){
 
-}
+    public void decode() {
 
+    }
 
+    public void execute() {
 
-
-
-
-
+    }
 
 
 }
